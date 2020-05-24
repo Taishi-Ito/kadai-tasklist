eaac6814a53e5a84ee47614cdf016147cb346a95
taishi.ito <taishi.ito1026@gmail.com>
2020-05-24 07:05:28 +0000

    last

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,10 @@
 class TasksController < ApplicationController
   
    before_action :require_user_logged_in
-    before_action :correct_user, only: [:destroy,:update, :edit, :show, :update,]
+    before_action :correct_user, only: [:destroy,:update, :edit, :show]
   
   def index
-      @task = current_user.tasks.build
+    
       @tasks = current_user.tasks.order(id: :desc).page(params[:page])
   end
 
